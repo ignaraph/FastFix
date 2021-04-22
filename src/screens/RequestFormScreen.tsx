9cf33c29e6460bc54ec78fe45813d4f62558058c
ignaraph <ignacioraphael@hotmail.com>
2021-04-22 12:30:12 +0200

    VideoCamera also working - Submit boton done but not working

--- src/screens/RequestFormScreen.tsx
+++ src/screens/RequestFormScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Text, View, StyleSheet, Image } from 'react-native';
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import Textarea from 'react-native-textarea';
 
 const takePhoto = () => {
   launchCamera({
@@ -9,25 +10,46 @@ const takePhoto = () => {
     cameraType: 'back',
     quality: 0.5
 
-  }, (resp) => console.log(resp))
+  }, (res) => console.log(res))
+}
+
+const filmVideo = () => {
+  launchCamera({
+    mediaType: 'video',
+    cameraType: 'back',
+    quality: 0.5
+  }, (res) => console.log(res))
 }
 
 export const RequestFormScreen = () => {
   return (
     <View style={styles.mainContainer}>
-      <Text style={styles.mainText}>Request a Quote!</Text>
-      <Text style={styles.formText}>Describe your problem below</Text>
-      <View style={styles.describeForm}>
-      <TextInput style={styles.inputForm} placeholder="write here"/>
-      </View>
-      <Text style={styles.formText}>Upload an image or video of the issue</Text>
+      <Text style={styles.mainTitle}>Request a Quote!</Text>
+      <Text style={styles.formTitle}>Describe your problem below</Text>
       <View>
+      <Textarea
+       containerStyle={styles.textareaContainer}
+       style={styles.textarea}
+      />
+      </View>
+      <Text style={styles.formTitle}>Upload an image or video of the issue</Text>
+      <View style={styles.btnContainer}>
         <TouchableOpacity
         onPress={takePhoto}
         >
-        <Image source={require('../assets/images/uploadIcon.jpg')}/>
+        <Image style={styles.cameraBtn} source={require('../assets/images/cameraIcon.jpg')}/>
+        </TouchableOpacity>
+        <TouchableOpacity
+        onPress={filmVideo}
+        >
+        <Image style={styles.cameraBtn} source={require('../assets/images/videoCameraIcon2.jpg')}/>
         </TouchableOpacity>
       </View>
+      <View style={styles.submitContainer}>
+        <TouchableOpacity>
+          <Text style={styles.submitBtn}>Submit Request</Text>
+        </TouchableOpacity>
+        </View>
       </View>
   )
 }
@@ -38,30 +60,65 @@ const styles = StyleSheet.create({
       backgroundColor: '#333232'
     },
 
-    mainText: {
-      color: '#ecf0f1',
+    mainTitle: {
+      color: '#FFAA2B',
       fontSize: 40,
       textAlign: 'center',
       marginTop: 10,
+      fontWeight: 'bold',
     },
 
-    formText: {
-      color: '#ecf0f1',
-      fontSize: 30,
+    formTitle: {
+      color: '#FFAA2B',
+      fontSize: 35,
       textAlign: 'center',
-      marginTop: 20,
+      marginVertical: 20,
     },
 
-    inputForm: {
-      marginVertical: 10,
-      backgroundColor: 'white',
-      width: 350,
-      height: 350,
+    textareaContainer: {
+      height: 250,
+      padding: 5,
+      backgroundColor: '#FFAA2B',
+    },
+    textarea: {
+      textAlignVertical: 'top',  // hack android
+      height: 170,
+      fontSize: 30,
+      color: '#333',
     },
 
-    describeForm: {
+    btnContainer: {
+      justifyContent: 'center',
       alignItems: 'center',
+      flexDirection: 'row',
+    },
+
+    cameraBtn: {
+      width: 100,
+      height: 100,
+      borderWidth: 20,
+      borderRadius: 50,
+      marginHorizontal: 10,
+    },
+
+    submitContainer: {
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      marginVertical: 160,
+    },
+
+    submitBtn: {
+      fontSize: 30,
+      borderRadius: 30,
+      backgroundColor: '#FFAA2B',
+      padding: 10,
+      borderColor: '#ecf0f1',
+      borderWidth: 4,
+      textAlign: 'center'
     }
+
+
+
 
 
 
