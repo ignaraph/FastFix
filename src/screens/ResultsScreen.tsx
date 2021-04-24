f4faec2e1f913fa4042dcff5a4af178704a9d888
ignaraph <ignacioraphael@hotmail.com>
2021-04-24 09:31:34 +0200

    Added files to gitignore

--- src/screens/ResultsScreen.tsx
+++ src/screens/ResultsScreen.tsx
@@ -26,7 +26,7 @@ export const ResultsScreen = ({route, navigation}: Props) => {
   // 192.168.1.194 This is Codeworks
   const fetchData = async () => {
     try {
-      const response = await fetch(`http://192.168.1.194:3002/${params.type}`)
+      const response = await fetch(`http://192.168.1.24:3002/${params.type}`)
       const result = await response.json();
       setContractor(result)
     } catch (error) {
